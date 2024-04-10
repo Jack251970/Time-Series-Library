657a50c51862a78c422b8d0438351d0798fb6bd6
庭茂 叶 <1160210343@qq.com>
2024-04-10 09:33:06 +0800

    适配

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from hyper_optimizer.optimizer import HyperOptimizer
-from run_optimizer import prepare_config, build_setting, build_config_dict, get_model_id_tags
+from run_optimizer import prepare_config, build_setting, build_config_dict, get_model_id_tags, set_args
 
 if __name__ == "__main__":
     h = HyperOptimizer(True, None,
-                       prepare_config, build_setting, build_config_dict, None, None,
-                       get_model_id_tags=get_model_id_tags)
+                       prepare_config, build_setting, build_config_dict, set_args, None, None,
+                       get_model_id_tags=get_model_id_tags, check_jump_experiment=None)
     h.start_search()
