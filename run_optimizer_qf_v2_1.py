3f9df7360c6bfdffbf4114ad9ae3e9b8c118966e
庭茂 叶 <1160210343@qq.com>
2024-04-23 18:22:33 +0800

    测试

--- run_optimizer_qf_v2_1.py
+++ run_optimizer_qf_v2_1.py
@@ -252,8 +252,7 @@ def combine_lists(lists, separator='_'):
 h = HyperOptimizer(False, ['LSTM-ED-CQ'],
                    prepare_config, build_setting, build_config_dict, set_args, get_fieldnames, get_search_space,
                    link_fieldnames_data=link_fieldnames_data)
-h.config_optimizer_settings(custom_test_time="", scan_all_csv=True, try_model=False, force_exp=False, add_tags=[],
-                            inverse_exp=True)
+h.config_optimizer_settings(custom_test_time="", scan_all_csv=True, try_model=False, force_exp=False, add_tags=[])
 
 if __name__ == "__main__":
     h.start_search(0)
