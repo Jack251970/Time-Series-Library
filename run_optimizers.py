a01982b802d1abdb639d43e2c684db1d01ac0412
庭茂 叶 <1160210343@qq.com>
2024-05-01 14:40:33 +0800

    测试

--- run_optimizers.py
+++ run_optimizers.py
@@ -1,8 +1,12 @@
 from optimizers.optimizer_qf_v2_1 import h as h1
 from optimizers.optimizer_qf_v2_2 import h as h2
 from optimizers.optimizer_qf_v2_3 import h as h3
+from optimizers.optimizer_qf_v2_4 import h as h4
+
+process_index = 0
 
 if __name__ == "__main__":
-    h1.start_search(0)
-    h2.start_search(0)
-    h3.start_search(0)
+    h1.start_search(process_index=process_index)
+    h2.start_search(process_index=process_index)
+    h3.start_search(process_index=process_index)
+    h4.start_search(process_index=process_index)
