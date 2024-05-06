9f56b8103230e2c3d45b563dc50d15913528eae7
庭茂 叶 <1160210343@qq.com>
2024-05-06 23:25:12 +0800

    整理 测试

--- optimizers/optimizer_qf_v2_1.py
+++ optimizers/optimizer_qf_v2_1.py
@@ -10,9 +10,6 @@ def link_fieldnames_data(_config):
         _config['enc_in'] = 321
         _config['dec_in'] = 321
         _config['c_out'] = 321
-
-        _config['n_heads'] = 8
-        _config['d_model'] = 64
     elif (_data_path == 'ETT-small/ETTh1.csv' or _data_path == 'ETT-small/ETTh2.csv' or
           _data_path == 'ETT-small/ETTm1.csv' or _data_path == 'ETT-small/ETTm2.csv'):
         # ETT dataset
@@ -24,9 +21,6 @@ def link_fieldnames_data(_config):
         _config['enc_in'] = 8
         _config['dec_in'] = 8
         _config['c_out'] = 8
-
-        _config['n_heads'] = 2
-        _config['d_model'] = 64
     elif _data_path == 'illness/national_illness.csv':
         # illness dataset
         _config['enc_in'] = 7
@@ -42,9 +36,6 @@ def link_fieldnames_data(_config):
         _config['enc_in'] = 21
         _config['dec_in'] = 21
         _config['c_out'] = 21
-
-        _config['n_heads'] = 4
-        _config['d_model'] = 40
     elif _data_path == 'pvod/station00.csv':
         # solar dataset
         _config['target'] = 'power'
@@ -100,8 +91,7 @@ def get_search_space():
         #                                             'traffic/traffic.csv', 'weather/weather.csv']},
 
         # need
-        'data_path': {'_type': 'choice', '_value': ['electricity/electricity.csv', 'exchange_rate/exchange_rate.csv',
-                                                    'weather/weather.csv']},
+        'data_path': {'_type': 'choice', '_value': ['electricity/electricity.csv', 'exchange_rate/exchange_rate.csv']},
     }
 
     learning_config = {
@@ -112,11 +102,7 @@ def get_search_space():
     period_config = {
         'seq_len': {'_type': 'single', '_value': 96},
         'label_len': {'_type': 'single', '_value': 16},
-        'pred_len': {'_type': 'single', '_value': 16},
-        # 'pred_len': {'_type': 'single', '_value': 32},
-        # 'pred_len': {'_type': 'single', '_value': 96},
-        # 'pred_len': {'_type': 'single', '_value': 192},
-        # 'pred_len': {'_type': 'choice', '_value': [16, 32, 96, 192]},
+        'pred_len': {'_type': 'single', '_value': 64},
         'e_layers': {'_type': 'single', '_value': 1},
         'd_layers': {'_type': 'single', '_value': 1},
     }
@@ -179,16 +165,16 @@ def get_search_space():
         'sample_times': {'_type': 'single', '_value': 99},
 
         # Step 1: Attention
-        # 'lstm_hidden_size': {'_type': 'single', '_value': 40},
-        # 'lstm_layers': {'_type': 'single', '_value': 1},
-        # 'n_heads': {'_type': 'choice', '_value': [1, 2, 4, 8]},
-        # 'd_model': {'_type': 'choice', '_value': [24, 40, 64]},
+        'lstm_hidden_size': {'_type': 'single', '_value': 40},
+        'lstm_layers': {'_type': 'single', '_value': 1},
+        'n_heads': {'_type': 'choice', '_value': [1, 2, 4, 8]},
+        'd_model': {'_type': 'choice', '_value': [24, 40, 64]},
 
         # Step 2: LSTM
-        'n_heads': {'_type': 'single', '_value': 4},
-        'd_model': {'_type': 'single', '_value': 24},
-        'lstm_hidden_size': {'_type': 'choice', '_value': [24, 40, 64]},
-        'lstm_layers': {'_type': 'choice', '_value': [1, 2, 3]},
+        # 'n_heads': {'_type': 'single', '_value': 4},
+        # 'd_model': {'_type': 'single', '_value': 24},
+        # 'lstm_hidden_size': {'_type': 'choice', '_value': [24, 40, 64]},
+        # 'lstm_layers': {'_type': 'choice', '_value': [1, 2, 3]},
 
         'custom_params': {'_type': 'single', '_value': 'AA_attn_dhz_ap1_norm'},
         # 'custom_params': {'_type': 'choice', '_value': build_custom_parameters()},
@@ -239,5 +225,5 @@ def get_search_space():
 
 h = HyperParameterOptimizer(script_mode=False, models=['LSTM-ED-CQ'],
                             get_search_space=get_search_space, link_fieldnames_data=link_fieldnames_data)
-h.config_optimizer_settings(root_path='.', data_csv_file='data_parameter_16_3.csv', scan_all_csv=False,
+h.config_optimizer_settings(root_path='.', data_csv_file='data_parameter_64_3.csv', scan_all_csv=False,
                             try_model=False, force_exp=False)
--- optimizers/optimizer_qf_v2_2.py
+++ optimizers/optimizer_qf_v2_2.py
@@ -10,9 +10,6 @@ def link_fieldnames_data(_config):
         _config['enc_in'] = 321
         _config['dec_in'] = 321
         _config['c_out'] = 321
-
-        _config['lstm_hidden_size'] = 40
-        _config['lstm_layers'] = 3
     elif (_data_path == 'ETT-small/ETTh1.csv' or _data_path == 'ETT-small/ETTh2.csv' or
           _data_path == 'ETT-small/ETTm1.csv' or _data_path == 'ETT-small/ETTm2.csv'):
         # ETT dataset
@@ -24,9 +21,6 @@ def link_fieldnames_data(_config):
         _config['enc_in'] = 8
         _config['dec_in'] = 8
         _config['c_out'] = 8
-
-        _config['lstm_hidden_size'] = 64
-        _config['lstm_layers'] = 3
     elif _data_path == 'illness/national_illness.csv':
         # illness dataset
         _config['enc_in'] = 7
@@ -42,9 +36,6 @@ def link_fieldnames_data(_config):
         _config['enc_in'] = 21
         _config['dec_in'] = 21
         _config['c_out'] = 21
-
-        _config['lstm_hidden_size'] = 64
-        _config['lstm_layers'] = 1
     elif _data_path == 'pvod/station00.csv':
         # solar dataset
         _config['target'] = 'power'
@@ -100,8 +91,7 @@ def get_search_space():
         #                                             'traffic/traffic.csv', 'weather/weather.csv']},
 
         # need
-        'data_path': {'_type': 'choice', '_value': ['electricity/electricity.csv', 'exchange_rate/exchange_rate.csv',
-                                                    'weather/weather.csv']},
+        'data_path': {'_type': 'choice', '_value': ['electricity/electricity.csv', 'exchange_rate/exchange_rate.csv']},
     }
 
     learning_config = {
@@ -112,11 +102,7 @@ def get_search_space():
     period_config = {
         'seq_len': {'_type': 'single', '_value': 96},
         'label_len': {'_type': 'single', '_value': 16},
-        # 'pred_len': {'_type': 'single', '_value': 16},
-        'pred_len': {'_type': 'single', '_value': 32},
-        # 'pred_len': {'_type': 'single', '_value': 96},
-        # 'pred_len': {'_type': 'single', '_value': 192},
-        # 'pred_len': {'_type': 'choice', '_value': [16, 32, 96, 192]},
+        'pred_len': {'_type': 'single', '_value': 64},
         'e_layers': {'_type': 'single', '_value': 1},
         'd_layers': {'_type': 'single', '_value': 1},
     }
@@ -175,17 +161,20 @@ def get_search_space():
         'learning_rate': {'_type': 'single', '_value': 0.001},
         'train_epochs': {'_type': 'single', '_value': 50},
 
+        'num_spline': {'_type': 'single', '_value': 20},
+        'sample_times': {'_type': 'single', '_value': 99},
+
         # Step 1: LSTM
-        # 'n_heads': {'_type': 'single', '_value': 2},
-        # 'd_model': {'_type': 'single', '_value': 24},
-        # 'lstm_hidden_size': {'_type': 'choice', '_value': [24, 40, 64]},
-        # 'lstm_layers': {'_type': 'choice', '_value': [1, 2, 3]},
+        'n_heads': {'_type': 'single', '_value': 2},
+        'd_model': {'_type': 'single', '_value': 24},
+        'lstm_hidden_size': {'_type': 'choice', '_value': [24, 40, 64]},
+        'lstm_layers': {'_type': 'choice', '_value': [1, 2, 3]},
 
         # Step 2: Attention
-        'lstm_hidden_size': {'_type': 'single', '_value': 40},
-        'lstm_layers': {'_type': 'single', '_value': 2},
-        'n_heads': {'_type': 'choice', '_value': [1, 2, 4, 8]},
-        'd_model': {'_type': 'choice', '_value': [24, 40, 64]},
+        # 'lstm_hidden_size': {'_type': 'single', '_value': 40},
+        # 'lstm_layers': {'_type': 'single', '_value': 2},
+        # 'n_heads': {'_type': 'choice', '_value': [1, 2, 4, 8]},
+        # 'd_model': {'_type': 'choice', '_value': [24, 40, 64]},
 
         'custom_params': {'_type': 'single', '_value': 'AA_attn_dhz_ap1_norm'},
     }
@@ -202,5 +191,5 @@ def get_search_space():
 
 h = HyperParameterOptimizer(script_mode=False, models=['LSTM-ED-CQ'],
                             get_search_space=get_search_space, link_fieldnames_data=link_fieldnames_data)
-h.config_optimizer_settings(root_path='.', data_csv_file='data_parameter_32_2.csv', scan_all_csv=False,
+h.config_optimizer_settings(root_path='.', data_csv_file='data_parameter_64_2.csv', scan_all_csv=False,
                             try_model=False, force_exp=False)
