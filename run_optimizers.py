cf9b24c26b597c40a6bd21b5f3be11e6a319d5a3
Jack251970 <1160210343@qq.com>
2024-05-07 10:25:50 +0800

    测试

--- run_optimizers.py
+++ run_optimizers.py
@@ -2,6 +2,10 @@ from optimizers.optimizer_qf_v2_1 import h as h1
 from optimizers.optimizer_qf_v2_2 import h as h2
 from optimizers.optimizer_qf_v2_3 import h as h3
 from optimizers.optimizer_qf_v2_4 import h as h4
+
+from optimizers.optimizer_transformer import h as hf
+from optimizers.optimizer_qsqf_c import h as hc
+
 from optimizers.optimizer_qf_test import h as ht
 
 process_index = 0
@@ -11,4 +15,8 @@ if __name__ == "__main__":
     h2.start_search(process_index=process_index)
     h3.start_search(process_index=process_index)
     h4.start_search(process_index=process_index)
+
+    hf.start_search(process_index=process_index)
+    hc.start_search(process_index=process_index)
+
     ht.start_search(process_index=process_index, force_test=True)
