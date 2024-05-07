da021b47ea266dc0671f6e70be71b5fcce06c081
庭茂 叶 <1160210343@qq.com>
2024-05-07 23:17:09 +0800

    测试

--- optimizers/optimizer_qf_test.py
+++ optimizers/optimizer_qf_test.py
@@ -123,7 +123,7 @@ def get_search_space():
     period_config = {
         'seq_len': {'_type': 'single', '_value': 96},
         'label_len': {'_type': 'choice', '_value': 16},
-        'pred_len': {'_type': 'choice', '_value': [16, 32, 64]},
+        'pred_len': {'_type': 'choice', '_value': [16, 32, 64, 96]},
         'e_layers': {'_type': 'single', '_value': 1},
         'd_layers': {'_type': 'single', '_value': 1},
     }
@@ -173,7 +173,7 @@ def get_search_space():
 
 h = HyperParameterOptimizer(script_mode=False, models=['LSTM-ED-CQ', 'QSQF-C'],
                             get_search_space=get_search_space, link_fieldnames_data=link_fieldnames_data)
-h.config_optimizer_settings(root_path='.', scan_all_csv=False, try_model=False, force_exp=False,
+h.config_optimizer_settings(root_path='.', scan_all_csv=False, try_model=False, force_exp=False, save_process=False,
                             custom_test_time=[
                                 # LSTM-AQ
                                 '2024-04-23 10-33-28',  # Electric_16
