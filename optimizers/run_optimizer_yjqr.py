be73544f53577f135c9f06e82d063b4997a544ac
庭茂 叶 <1160210343@qq.com>
2024-05-27 23:20:18 +0800

    测试

--- optimizers/run_optimizer_yjqr.py
+++ optimizers/run_optimizer_yjqr.py
@@ -154,7 +154,7 @@ def get_search_space():
 
         'learning_rate': {'_type': 'single', '_value': 0.001},
         # 'train_epochs': {'_type': 'single', '_value': 20},
-        'train_epochs': {'_type': 'single', '_value': 50},
+        'train_epochs': {'_type': 'single', '_value': 30},
         # 'train_epochs': {'_type': 'choice', '_value': [20, 50]},
 
         'lstm_hidden_size': {'_type': 'single', '_value': 40},
@@ -172,7 +172,7 @@ def get_search_space():
     return [default_config, dataset_config, learning_config, period_config], model_configs
 
 
-h = HyperParameterOptimizer(script_mode=False, models=['LSTM-ED-YJQR'],
+h = HyperParameterOptimizer(script_mode=False, models=['LSTM-YJQR', 'LSTM-ED-YJQR'],
                             get_search_space=get_search_space, link_fieldnames_data=link_fieldnames_data)
 h.config_optimizer_settings(root_path='..', data_csv_file='data_yjqr.csv', scan_all_csv=False,
                             try_model=False, force_exp=True)
