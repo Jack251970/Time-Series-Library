1158af6f76847a2e4696730d226e08c4b3489f76
庭茂 叶 <1160210343@qq.com>
2024-06-02 14:46:58 +0800

    优化测试脚本

--- analyze/analyze_loss.py
+++ analyze/analyze_loss.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 import matplotlib.pyplot as plt
 
 from analyze.test_data_factory import get_loss
--- analyze/analyze_metrics.py
+++ analyze/analyze_metrics.py
@@ -10,16 +10,14 @@ crps_lstm_aq, crps_steps_lstm_aq, mre_lstm_aq, pinaw_lstm_aq, pinaw_steps_lstm_a
 qsqf_c_exp_name = 'QSQF-C_Electricity_96'
 crps_qsqf_c, crps_steps_qsqf_c, mre_qsqf_c, pinaw_qsqf_c, pinaw_steps_qsqf_c = get_prob_metrics(qsqf_c_exp_name)
 
-config_row = get_config_row(qsqf_c_exp_name)
-pred_len = int(config_row['pred_len'])
+config_row_lstm_aq = get_config_row(lstm_aq_exp_name)
+pred_len = int(config_row_lstm_aq['pred_len'])
 
 x_data = range(1, pred_len, 1)
 
 plt.clf()
-if crps_steps_lstm_aq is not None:
-    plt.plot(x_data, crps_steps_lstm_aq[1:], 'bo-', alpha=0.5, linewidth=1, label='LSTM-AQ')
-if crps_steps_qsqf_c is not None:
-    plt.plot(x_data, crps_steps_qsqf_c[1:], 'ro-', alpha=0.5, linewidth=1, label='QSQF-C')
+plt.plot(x_data, crps_steps_lstm_aq[1:], 'bo-', alpha=0.5, linewidth=1, label='LSTM-AQ')
+plt.plot(x_data, crps_steps_qsqf_c[1:], 'ro-', alpha=0.5, linewidth=1, label='QSQF-C')
 
 plt.legend()
 plt.xlabel('Prediction Step')
@@ -27,10 +25,8 @@ plt.ylabel('CRPS')
 plt.savefig('CRPS_steps.png')
 
 plt.clf()
-if pinaw_steps_lstm_aq is not None:
-    plt.plot(x_data, pinaw_steps_lstm_aq[1:], 'bo-', alpha=0.5, linewidth=1, label='LSTM-AQ')
-if pinaw_steps_qsqf_c is not None:
-    plt.plot(x_data, pinaw_steps_qsqf_c[1:], 'ro-', alpha=0.5, linewidth=1, label='QSQF-C')
+plt.plot(x_data, pinaw_steps_lstm_aq[1:], 'bo-', alpha=0.5, linewidth=1, label='LSTM-AQ')
+plt.plot(x_data, pinaw_steps_qsqf_c[1:], 'ro-', alpha=0.5, linewidth=1, label='QSQF-C')
 
 plt.legend()
 plt.xlabel('Prediction Step')
