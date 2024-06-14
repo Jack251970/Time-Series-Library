6dc1d23cfbae0c9eddf1e9f8c7aa4a7949f19354
庭茂 叶 <1160210343@qq.com>
2024-06-15 01:53:47 +0800

    测试

--- run_lstm_aq_comp_optimizer.py
+++ run_lstm_aq_comp_optimizer.py
@@ -1,8 +1,12 @@
 from optimizers.optimizer_qf_comp import h
 from optimizers.optimizer_qf_comp1 import h as h1
+from optimizers.optimizer_qf_comp_test import h as ht
+from optimizers.optimizer_qf_comp1_test import h as h1t
 
 process_index = 0
 
 if __name__ == "__main__":
     h.start_search(process_index=process_index)
-    h1.start_search(process_index=process_index, shutdown_after_done=False)
+    h1.start_search(process_index=process_index)
+    ht.start_search(process_index=process_index)
+    h1t.start_search(process_index=process_index, shutdown_after_done=True)
