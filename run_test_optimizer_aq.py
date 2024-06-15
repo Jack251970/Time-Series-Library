2095b349f0840da1d1cb4bade3bb629f55c0dfc4
庭茂 叶 <1160210343@qq.com>
2024-06-15 20:53:05 +0800

    测试

--- run_test_optimizer_aq.py
+++ run_test_optimizer_aq.py
@@ -1,6 +1,8 @@
 from optimizers.optimizer_qf_test import h as ht
+from optimizers.optimizer_qsqf_test import h as ht1
 
 process_index = 0
 
 if __name__ == "__main__":
     ht.start_search(process_index=process_index, force_test=True)
+    ht1.start_search(process_index=process_index, force_test=True)
