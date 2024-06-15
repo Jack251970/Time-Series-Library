ae654fd206e0362b377cb842044c0fff0501f560
庭茂 叶 <1160210343@qq.com>
2024-06-15 21:09:04 +0800

    测试

--- run_qsqf_optimizer.py
+++ run_qsqf_optimizer.py
@@ -3,4 +3,4 @@ from optimizers.optimizer_qsqf import h as hc
 process_index = 0
 
 if __name__ == "__main__":
-    hc.start_search(process_index=process_index)
+    hc.start_search(process_index=process_index, shutdown_after_done=False)
