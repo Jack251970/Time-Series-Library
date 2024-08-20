0d05044969cc454fbc79ce9ca20488dc71902f7c
Jack251970 <1160210343@qq.com>
2024-08-20 23:43:48 +0800

    测试

--- optimizers/optimizer_qf_test.py
+++ optimizers/optimizer_qf_test.py
@@ -153,7 +153,8 @@ def get_search_space():
         'data': {'_type': 'single', '_value': 'custom'},
         'features': {'_type': 'single', '_value': 'MS'},
         'root_path': {'_type': 'single', '_value': './dataset/'},
-        'data_path': {'_type': 'single', '_value': 'electricity/electricity.csv'},
+        # 'data_path': {'_type': 'single', '_value': 'electricity/electricity.csv'},
+        'data_path': {'_type': 'choice', '_value': ['electricity/electricity.csv', 'exchange_rate/exchange_rate.csv']},
         # 'data_path': {'_type': 'choice', '_value': ['electricity/electricity.csv', 'exchange_rate/exchange_rate.csv',
         #                                             'traffic/traffic.csv']},
     }
@@ -166,8 +167,8 @@ def get_search_space():
     period_config = {
         'seq_len': {'_type': 'single', '_value': 96},
         'label_len': {'_type': 'choice', '_value': 16},
-        # 'pred_len': {'_type': 'single', '_value': 96},
-        'pred_len': {'_type': 'choice', '_value': [16, 32, 64, 96]},
+        'pred_len': {'_type': 'single', '_value': 96},
+        # 'pred_len': {'_type': 'choice', '_value': [16, 32, 64, 96]},
         'e_layers': {'_type': 'single', '_value': 1},
         'd_layers': {'_type': 'single', '_value': 1},
     }
@@ -209,14 +210,18 @@ def get_search_space():
 
     model_configs = {
         'LSTM-AQ': lstm_aq_config,
-        'AL-QSQF': al_qsqf_config
+        'AL-QSQF': al_qsqf_config,
+        'LSTM-AQ1': lstm_aq_config,
+        'LSTM-AQ2': lstm_aq_config,
+        'LSTM-AQ3': lstm_aq_config,
+        'LSTM-AQ4': lstm_aq_config,
     }
 
     return [default_config, dataset_config, learning_config, period_config], model_configs
 
 
-h = HyperParameterOptimizer(script_mode=False, models=['LSTM-AQ'],
+h = HyperParameterOptimizer(script_mode=False, models=['LSTM-AQ', 'LSTM-AQ1', 'LSTM-AQ2', 'LSTM-AQ3', 'LSTM-AQ4'],
                             get_search_space=get_search_space, link_fieldnames_data=link_fieldnames_data,
                             get_custom_test_time=get_custom_test_time)
-h.config_optimizer_settings(root_path='.', data_csv_file='data_test.csv',
+h.config_optimizer_settings(root_path='.', data_csv_file='data_comp_lf.csv',
                             scan_all_csv=False, try_model=False, force_exp=True, save_process=False)
