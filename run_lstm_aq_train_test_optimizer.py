73dd80e3e7c31f1a5562404aeac493979d24bc38
Jack251970 <1160210343@qq.com>
2024-08-24 20:34:27 +0800

    测试

--- run_lstm_aq_train_test_optimizer.py
+++ run_lstm_aq_train_test_optimizer.py
@@ -3,4 +3,6 @@ from optimizers.optimizer_qf_train_test import h as h
 process_index = 0
 
 if __name__ == "__main__":
+    for i in range(1, 6, 1):
+        h.start_search(process_index=process_index, shutdown_after_done=False)
     h.start_search(process_index=process_index, shutdown_after_done=False)
